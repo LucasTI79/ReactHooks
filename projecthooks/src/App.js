b03fd34c768adc409fa1af23173e7a63ac84d631
Lucas <lukasalves271@gmail.com>
2021-04-25 15:42:52 -0300

    add useMemo

--- projecthooks/src/App.js
+++ projecthooks/src/App.js
@@ -3,36 +3,36 @@ import './App.css';
 import Button from './components/Button';
 import ThemeProvider, { useTheme } from './contexts/ThemeContext'
 
-function reducer(state, action){
-  switch(action.type){
-    case 'plus':
-      return {
-        counter: state.counter + 1,
-        clicks: state.clicks + 1
-      }
-    case 'minus':
-      return {
-        counter: state.counter - 1,
-        clicks: state.clicks + 1
-      }
-    default:
-      return state
-  }
-  
-}
-
-const initialValue = {
-  counter: 0,
-  clicks: 0
-}
-
 const fnPlusCounter = new Set()
 
 const fnMinusCounter = new Set()
 
 function App() {
-  const [name, setName] =  React.useState('')
+  function reducer(state, action){
+    switch(action.type){
+      case 'plus':
+        return {
+          counter: state.counter + 1,
+          clicks: state.clicks + 1
+        }
+      case 'minus':
+        return {
+          counter: state.counter - 1,
+          clicks: state.clicks + 1
+        }
+      default:
+        return state
+    }
+    
+  }
+  
+  const initialValue = {
+    counter: 0,
+    clicks: 0
+  }
 
+  const [name, setName] =  React.useState('')
+  const [renderMemo, setRenderMemo] = React.useState(0)
   const [state, dispatch] = React.useReducer(reducer, initialValue);
 
   React.useEffect(()=>{
@@ -47,25 +47,38 @@ function App() {
     )
   },[])
 
-  const handlePlus = React.useCallback(() => {
-    dispatch({ type: 'plus' })
-  },[])
-
-  fnPlusCounter.add(handlePlus)
-  console.log(fnPlusCounter.size)
-
   function handleMinus(){
     dispatch({ type: 'minus'})
   }
   fnMinusCounter.add(handleMinus)
   console.log(fnMinusCounter.size)
-  
+
+  //useCallback return function
+  const handlePlus = React.useCallback(() => {
+    dispatch({ type: 'plus' })
+  },[])
+  fnPlusCounter.add(handlePlus)
+  console.log(fnPlusCounter.size)
+
+  //useMemo execute function
+  const total = React.useMemo(() => {
+    console.log('useMemo rodou')
+    return (renderMemo * 10)
+  },[renderMemo])
+
+  function handleEqual(){
+    setRenderMemo(prev => prev+1)
+  }
+
   return (
     <ThemeProvider>
       <div className="App">
         <h1>{state.counter}</h1>
         <h4>Cliques: {state.clicks}</h4>
+        <h5>Cliques equal: {renderMemo}</h5>
+        <h6>Total: {total}</h6>
         <button disabled={!state.counter && true} onClick={handleMinus}>-</button>
+        <button onClick={handleEqual}>=</button>
         <Plus onClick={handlePlus}>+</Plus>
 
         <br/>
